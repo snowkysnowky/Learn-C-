1962be7bba8b9d0989fdd6a7f53bf28290b909e7
DAFFA <snowkysnowky01@gmail.com>
2023-01-27 11:16:16 +0700

    commit

---
+++ cin-test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nama;
+    cout << "Tuliskan nama: ";
+    cin >> nama;
+
+    cout << "Hi " << nama;
+    cout << " Apa kabar?" <<endl;
+
+
+    return 0;
+}
---
+++ test_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int file_size;  //Snake case
+    int FileSize;   //Pascal case
+    int fileSize;   //Camel case
+    int iFileSize;  //Hungarian notation
+
+
+    return 0;
+}
